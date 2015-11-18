e10fbc527683c5ceb6ad863c7fe19ae9b45078d0
keltheceo <keltheceo@gmail.com>
2015-11-18 14:28:11 -0800

    Added updates dir && models dir

---
+++ models/_User.js
@@ -0,0 +1,37 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * <%= userModel %> Model
+ * ==========
+ */
+
+var <%= userModel %> = new keystone.List('<%= userModel %>');
+
+<%= userModel %>.add({
+	name: { type: Types.Name, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, index: true },
+	password: { type: Types.Password, initial: true, required: true }
+}, 'Permissions', {
+	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
+});
+
+// Provide access to Keystone
+<%= userModel %>.schema.virtual('canAccessKeystone').get(function() {
+	return this.isAdmin;
+});
+
+<% if (includeBlog) { %>
+/**
+ * Relationships
+ */
+
+<%= userModel %>.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+
+<% } %>
+/**
+ * Registration
+ */
+
+<%= userModel %>.defaultColumns = 'name, email, isAdmin';
+<%= userModel %>.register();
